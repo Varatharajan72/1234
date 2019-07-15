c1288cfb162ad293df1268b2b314c11f72a70025
Varatharajan72 <51222666+Varatharajan72@users.noreply.github.com>
2019-07-14 23:49:49 -0700

    Update prime or not.py

--- prime or not.py
+++ prime or not.py
@@ -1,7 +1,7 @@
-number=int(input())
-if number>1:
-   for k in range(2,number):
-      if number%k==0:
+N=int(input())
+if N>1:
+   for k in range(2,N):
+      if N%k==0:
          print("no")
          break
    else:
