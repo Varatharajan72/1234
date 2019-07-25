c0f9ec4227812907600eb5cd83c9acdd5a0ce358
Varatharajan72 <51222666+Varatharajan72@users.noreply.github.com>
2019-07-25 16:43:55 +1000

    Create pl5.py

---
+++ pl5.py
@@ -0,0 +1,7 @@
+RN=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+N=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+w=(input())
+for k in range(20):
+  if(w==RN[k]):
+    print(N[k])
+   
